b83564a7c621b0780b57e168f2bbbfb44f18f4fb
spitchell <mr.spitchell@gmail.com>
2017-05-18 14:35:19 +0100

    input tracking and submission mechanisms done

--- script.js
+++ script.js
@@ -6,13 +6,29 @@ let data = {
 let controller = {
   init() {
     this.inputField = document.getElementById('inputField');
+    this.inputField.addEventListener('keypress', function(e) {
+        let key = e.which || e.keyCode; // what is this???:w
+        if (key === 13) {
+          controller.captureInput()
+        }
+    });
     this.inputField.addEventListener('input', this.trackInput);
+    this.randomButton = document.getElementById('mainButton');
+    // this.randomButton.addEventListener('click', this.getRandom);
+    
   },
 
   trackInput(e) {
-    let currentInput = e.target.value;
-    console.log(currentInput);
+    // let currentInput = e.target.value;
+    data.currentInput = e.target.value;
+    console.log(data.currentInput);
+  },
+
+  captureInput() {
+    console.log(data.currentInput);
+    
   }
+
 
 
 
