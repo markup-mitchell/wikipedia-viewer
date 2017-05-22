02268cb3b67f1f1e6d265cae85f635ffac894f9c
spitchell <mr.spitchell@gmail.com>
2017-05-22 09:11:49 +0100

    api query formation added. response parsed

--- script.js
+++ script.js
@@ -29,18 +29,38 @@ let controller = {
     controller.generateQuery();
   },
 
-  generateQuery() {
+  generateQuery() { // not separating the generate query from send query feels like a mistake
     var proxyCORS = "https://cors-anywhere.herokuapp.com/";
     let searchTerms = data.currentInput;
     searchTerms = searchTerms.replace(/ /g, '%20');
     console.log(searchTerms);
-    fetch(proxyCORS + `https://en.wikipedia.org/w/api.php?action=query&titles=${searchTerms}&prop=revisions&rvprop=content&format=json`).then(function(response){
-      return response.blob()
-    }).then(function(myBlob) {
-      console.log(myBlob)
-    })
-  }
+    let xhrRequest = function() {
+        let xhr = new XMLHttpRequest();
+        // xhr.open('GET', proxyCORS + `https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro=&explaintext=&titles=${searchTerms})`);
+        xhr.open('GET', proxyCORS + `https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=${searchTerms}&format=json&utf8=`);
+        xhr.send(null);
+        xhr.onload = function() {
+            if (xhr.status === 200) {
+                var responseObj = JSON.parse(xhr.response);
+                console.log(responseObj.query.search);
+            }
+            else if (xhr.status === 404) {
+                console.log('Error, page not found');
+            }
+            else {
+                console.log('Error, status code: ' + xhr.status);
+            }
+        }
+    }
+  console.log(xhrRequest()); 
+    // fetch(proxyCORS + `https://en.wikipedia.org/w/api.php?action=query&titles=${searchTerms}&prop=revisions&rvprop=content&format=json`).then(function(response){
+    //   return response
+    // }).then(function(response) {
+    //   console.log(response)
+    // })
+  },
 
+parseResults(responseObj) {}
 
 
 
